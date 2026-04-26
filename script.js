06677a2f4eec2957c44d82bf730af995dad39de2
Conrad <conradkaranja5@gmail.com>
2026-04-26 21:15:06 +0300

    add changes to script.js and style.css

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function updateNav() {
     if (isLoggedIn()) {
       authLi.innerHTML = `<a href="#" class="logout-btn" onclick="logout()">Logout (${currentUser})</a>`;
     } else {
-      authLi.innerHTML = `<li><a href="login.html">Login</a></li>`;
+      authLi.innerHTML = `<li><a href="login.html" class="login-btn">Login</a></li>`;
     }
   }
 }
